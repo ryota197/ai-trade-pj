3d5f6180327636fe36f3a1205aa73eb93e3fecc0
ryota_macbookpro <rodin0510@icloud.com>
2026-01-31 14:08:56 +0900

    feat: create   services/constants/ layer
      - Move constants from domain/constants/ to services/constants/
      - Update internal imports
      - Part of simplified architecture refactoring

---
+++ backend/src/services/constants/__init__.py
@@ -0,0 +1,9 @@
+"""Service Constants"""
+
+from src.services.constants.canslim_defaults import CANSLIMDefaults
+from src.services.constants.trading_days import TradingDays
+
+__all__ = [
+    "CANSLIMDefaults",
+    "TradingDays",
+]
